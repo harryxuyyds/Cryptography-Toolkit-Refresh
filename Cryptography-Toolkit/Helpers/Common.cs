50c8ca4bb9df13a2999fc43e47c1b323d3bcdbc6
Harry Xu <harryxuyyds@outlook.com>
2025-06-18 23:49:27 +0800

    完善增加页面

--- Cryptography-Toolkit/Helpers/Common.cs
+++ Cryptography-Toolkit/Helpers/Common.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cryptography_Toolkit.Components
+namespace Cryptography_Toolkit.Helpers
 {
     public class Common
     {
@@ -23,5 +23,29 @@ namespace Cryptography_Toolkit.Components
             }
             return newRandom.ToString();
         }
+
+        public int GenerateRandomInt(int minValue, int maxValue)
+        {
+            Random rd = new Random();
+            return rd.Next(minValue, maxValue);
+        }
+
+        public int SquareMultiplyAlgorithmCalc(int smA, int smH, int smN)
+        {
+            string smHToBIn = Convert.ToString(smH, 2);
+            int smHLength = smHToBIn.Length;
+            int index;
+            long smResult = 1;
+            for (index = 1; index <= smHLength; index++)
+            {
+                smResult = smResult * smResult % smN;
+                if (smHToBIn[index - 1] == '1')
+                {
+                    smResult = smResult * smA % smN;
+                }
+            }
+            return (int)smResult;
+        }
+        
     }
 }
