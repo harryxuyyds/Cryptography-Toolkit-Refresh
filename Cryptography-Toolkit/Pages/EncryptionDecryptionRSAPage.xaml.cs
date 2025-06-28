a67b2565649dba36b349aef4e93893872d1ff569
Harry Xu <harryxuyyds@outlook.com>
2025-06-29 03:23:58 +0800

    RSA加密解密页面实现，修改了一些设置项图标

--- Cryptography-Toolkit/Pages/EncryptionDecryptionRSAPage.xaml.cs
+++ Cryptography-Toolkit/Pages/EncryptionDecryptionRSAPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Cryptography_Toolkit.Components;
+using Cryptography_Toolkit.Helpers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,8 +14,6 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-using Cryptography_Toolkit.Helpers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -40,7 +40,7 @@ namespace Cryptography_Toolkit.Pages
             var millerRabinPrimalityTest = new Components.MillerRabinPrimalityTest();
 
             if (int.TryParse(RsaPrimeGenerationParaPNumberBox.Text, out var p) &&
-                int.TryParse(RsaPrimeGenerationParaPNumberBox.Text, out var q) &&
+                int.TryParse(RsaPrimeGenerationParaQNumberBox.Text, out var q) &&
                 millerRabinPrimalityTest.MillerRabinPrimalityTestRun(8, p) &&
                 millerRabinPrimalityTest.MillerRabinPrimalityTestRun(8, q))
             {
@@ -51,6 +51,8 @@ namespace Cryptography_Toolkit.Pages
                 RsaKeyGenerationPublicExponentENumberBox.Maximum = _eulerPhiN - 1;
                 RsaKeyGenerationPublicExponentENumberBox.Value = _eulerPhiN - 1;
                 RsaKeyGenerationPublicExponentENumberBox.IsEnabled = true;
+                RsaPrimeGenerationSettingsExpander.IsExpanded = false;
+                RsaKeyGenerationSettingsExpander.IsExpanded = true;
             }
             else
             {
@@ -62,7 +64,7 @@ namespace Cryptography_Toolkit.Pages
         {
             if (int.TryParse(RsaKeyGenerationPublicExponentENumberBox.Text, out _keyPubE))
             {
-                (int gcd, int s, int t) = _common.ExtendedEuclideanAlgorithmCalc(_keyPubE, _eulerPhiN);
+                (int gcd, int s, int t) = _common.ExtendedEuclideanAlgorithmCalc(_eulerPhiN, _keyPubE);
                 if (gcd == 1)
                 {
                     _keyPrD = t;
@@ -71,6 +73,12 @@ namespace Cryptography_Toolkit.Pages
                         _keyPrD += _eulerPhiN;
                     }
                     RsaKeyGenerationLogTextBox.Text = $"Public Key: k_pub(n, e) = ({_keyPubN}, {_keyPubE})\nPrivate Key: k_pr(d) = ({_keyPrD})";
+                    RsaEncryptionPublicKeyNNumberBox.Value = _keyPubN;
+                    RsaEncryptionPublicKeyENumberBox.Value = _keyPubE;
+                    RsaDecryptionPublicKeyNNumberBox.Value = _keyPubN;
+                    RsaDecryptionPrivateKeyDNumberBox.Value = _keyPrD;
+                    RsaKeyGenerationSettingsExpander.IsExpanded = false;
+                    RsaEncryptionSettingsExpander.IsExpanded = true;
                 }
                 else
                 {
@@ -80,6 +88,41 @@ namespace Cryptography_Toolkit.Pages
             else
             {
                 RsaKeyGenerationLogTextBox.Text = "Please enter correct parameters.";
+            }
+        }
+
+        private void RsaEncryptionExecuteButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(RsaEncryptionPlaintextNumberBox.Text, out var plaintext) &&
+                int.TryParse(RsaEncryptionPublicKeyNNumberBox.Text, out var keyPubN) &&
+                int.TryParse(RsaEncryptionPublicKeyENumberBox.Text, out var keyPubE) &&
+                plaintext < keyPubN)
+            {
+                var ciphertext = _common.SquareMultiplyAlgorithmCalc(plaintext, keyPubE, keyPubN);
+                RsaEncryptionLogTextBox.Text = $"Plaintext: {plaintext}\nPublic Key (n, e): ({keyPubN}, {keyPubE})\nCiphertext: {ciphertext}";
+                RsaDecryptionCiphertextNumberBox.Value = ciphertext;
+                RsaEncryptionSettingsExpander.IsExpanded = false;
+                RsaDecryptionSettingsExpander.IsExpanded = true;
+            }
+            else
+            {
+                RsaEncryptionLogTextBox.Text = "Please enter correct parameters.";
+            }
+        }
+
+        private void RsaDecryptionExecuteButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(RsaDecryptionCiphertextNumberBox.Text, out var ciphertext) &&
+                int.TryParse(RsaDecryptionPublicKeyNNumberBox.Text, out var keyPubN) &&
+                int.TryParse(RsaDecryptionPrivateKeyDNumberBox.Text, out var keyPrD))
+            {
+                var plaintext = _common.SquareMultiplyAlgorithmCalc(ciphertext, keyPrD, keyPubN);
+                RsaDecryptionLogTextBox.Text = $"Ciphertext: {ciphertext}\nPublic Key (n): {keyPubN}\nPrivate Key (d): {keyPrD}\nPlaintext: {plaintext}";
+                RsaDecryptionSettingsExpander.IsExpanded = false;
+            }
+            else
+            {
+                RsaDecryptionLogTextBox.Text = "Please enter correct parameters.";
             }
         }
     }
